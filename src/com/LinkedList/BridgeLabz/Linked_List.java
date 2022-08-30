e78d14c7fc4714768989f8c70b40ab7b3809464e
Snehal <nanavaresnehal115@gmail.com>
2022-08-30 17:48:03 +0530

    ADDED[Snehal] UC-1 Created Simple Linked List

---
+++ src/com/LinkedList/BridgeLabz/Linked_List.java
@@ -0,0 +1,21 @@
+package com.LinkedList.BridgeLabz;
+
+/*
+ * UC1 - Created Simple Linked List of 56, 30, 70
+ */
+import java.util.LinkedList;
+
+public class Linked_List {    
+	 public static void main(String[] args) 
+	 {
+		 //Created Linked List 
+		    LinkedList<Integer> List = new LinkedList<>();
+
+		    // added elements in the linked list
+		    List.add(56);
+		    List.add(30);
+		    List.add(70);
+		    
+		    System.out.println("LinkedList: " + List);
+		  }    
+}    
